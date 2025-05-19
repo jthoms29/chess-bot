1994f446627452db4455030fcebe7b4581aaa5f3
jthoms29 <johnthoms567@gmail.com>
2025-05-18 20:32:01 -0600

    Started implementation of recursive minimax search

--- src/minimax.rs
+++ src/minimax.rs
@@ -1,74 +1,112 @@
 use std::collections::HashSet;
 use crate::state::{self, action_to_state, copy_state, generate_legal_moves, State};
-#[derive(Default)]
-pub struct MinimaxSearch {
-    max_best: i16,
-    min_best: i16,
-    depth_limit: u8,
+
+
+
+
+/* Returns the best move that can be taken given a state and depth-limit */
+pub fn search_max(cur_state: &State, depth_limit: u16) -> String {
+    let mut best_action: String = String::new();
+    let mut alpha: i16 = i16::MIN;
+    let mut beta:i16 = i16::MAX;
+    let mut best: i16 = i16::MIN;
+
+    let mut cur_val: i16;
+
+    let legal_moves: HashSet<String> = generate_legal_moves(&cur_state);
+    for action in &legal_moves {
+        let mut new_state = copy_state(&cur_state);
+        action_to_state(&mut new_state, &action);
+        cur_val = min_recurse(&new_state, alpha, beta, depth_limit-1);
+
+        if cur_val > best {
+            best = cur_val;
+            best_action = action.clone();
+        } 
+    }
+    return best_action;
 }
 
 
-impl MinimaxSearch {
+/* Returns the best move that can be taken given a state and depth-limit */
+pub fn search_min(cur_state: &State, depth_limit: u16) -> String {
+    let mut best_action: String = String::new();
+    let mut alpha: i16 = i16::MIN;
+    let mut beta:i16 = i16::MAX;
+    let mut best: i16 = i16::MIN;
 
-    pub fn new(depth_limit: u8) -> MinimaxSearch {
-        let search_struct = MinimaxSearch { 
-            max_best: (i16::MIN), 
-            min_best: (i16::MAX), 
-            depth_limit: (depth_limit) 
-        };
+    let mut cur_val: i16;
 
-        return search_struct;
+    let legal_moves: HashSet<String> = generate_legal_moves(&cur_state);
+    for action in &legal_moves {
+        let mut new_state = copy_state(&cur_state);
+        action_to_state(&mut new_state, &action);
+        cur_val = max_recurse(&new_state, alpha, beta, depth_limit-1);
+
+        if cur_val < best {
+            best = cur_val;
+            best_action = action.clone();
+        } 
     }
-
-    /* Returns the best move that can be taken given a state and depth-limit */
-    pub fn search_max(&self, cur_state: &State, depth_limit: u16) -> String {
-        let mut best_action: String = String::new();
-        let mut best_val: i16 = i16::MIN;
-        let mut cur_val: i16;
-
-        let legal_moves: HashSet<String> = generate_legal_moves(&cur_state);
-        for action in &legal_moves {
-            let mut new_state = copy_state(&cur_state);
-            action_to_state(&mut new_state, &action);
-            cur_val = min_recurse(&new_state, depth_limit-1)
-        }
-
-        return best;
-    }
-
-
-    fn max_recurse(&self, cur_state: &State, depth_limit: u16) -> i16 {
-        if State::victory_check(&cur_state) == -1 {
-            return -1000;
-        }
-        if State:: victory_check(&cur_state) == 1 {
-            return 1000;
-        }
-
-
-        let legal_moves: HashSet<String> = generate_legal_moves(&cur_state);
-
-        for action in &legal_moves {
-            let mut new_state = copy_state(&cur_state);
-            action_to_state(&mut new_state, &action);
-        }
-    }
-
-    fn min_recurse(&self, cur_state: &State, depth_limit: u16) -> i16 {
-        if State::victory_check(cur_state) == -1 {
-            return -1000;
-        }
-        if State:: victory_check(cur_state) == 1 {
-            return 1000;
-        }
-
-        let legal_moves: HashSet<String> = generate_legal_moves(&cur_state);
-
-        for action in &legal_moves {
-            let mut new_state = copy_state(&cur_state);
-            action_to_state(&mut new_state, &action);
-        }
-    }
-
-
+    return best_action;
 }
+
+
+fn max_recurse(cur_state: &State, alpha: i16, beta: i16, depth_limit: u16) -> i16 {
+    if State::victory_check(&cur_state) == -1 {
+        return -1000;
+    }
+    if State:: victory_check(&cur_state) == 1 {
+        return 1000;
+    }
+
+    if depth_limit == 0 {
+        return 0;
+    }
+
+    let mut cur_val: i16;
+    let mut best: i16 = i16::MIN;
+
+    let legal_moves: HashSet<String> = generate_legal_moves(&cur_state);
+    for action in &legal_moves {
+        let mut new_state = copy_state(&cur_state);
+        action_to_state(&mut new_state, &action);
+        cur_val = min_recurse(&new_state, alpha, beta, depth_limit-1);
+
+        if cur_val > best {
+            best = cur_val;
+        }
+    }
+    return best;
+}
+
+
+fn min_recurse(cur_state: &State, alpha: i16, beta: i16, depth_limit: u16) -> i16 {
+    if State::victory_check(cur_state) == -1 {
+        return -1000;
+    }
+    if State:: victory_check(cur_state) == 1 {
+        return 1000;
+    } 
+
+    if depth_limit == 0 {
+        return 0;
+    }
+
+    let mut cur_val: i16;
+    let mut best: i16 = i16::MAX;
+
+    let legal_moves: HashSet<String> = generate_legal_moves(&cur_state);
+
+    for action in &legal_moves {
+        let mut new_state = copy_state(&cur_state);
+        action_to_state(&mut new_state, &action);
+        cur_val = max_recurse(&new_state, alpha, beta, depth_limit-1);
+
+        if cur_val < best {
+            best = cur_val
+        }
+    }
+    return best;
+}
+
